b74d27510b0cc7421d3467581e6a576ecfc64dae
Petter Holmström <petter.holmstrom@abo.fi>
2012-04-01 17:30:16 +0300

    Fixed javadocs

--- eventbus/src/main/java/net/pkhsolutions/ceres/eventbus/EventBusHolder.java
+++ eventbus/src/main/java/net/pkhsolutions/ceres/eventbus/EventBusHolder.java
@@ -1,8 +1,23 @@
+/*
+ * Copyright (c) 2012 Petter Holmström
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.pkhsolutions.ceres.eventbus;
 
 /**
  * This class provides static methods for setting and accessing an event bus. It
- * can be used to simply code where multiple objects need access to the event
+ * can be used to simplify code where multiple objects need access to the event
  * bus. Instead of passing in the event bus instance to all objects, they can
  * look it up themselves. <p> Note, however, that if you are using a dependency
  * injection framework (such as CDI or Spring), it is probably better to use
@@ -38,7 +53,8 @@ public class EventBusHolder {
      * Sets the strategy to use for storing and retrieving the event bus
      * instance.
      *
-     * @param strategy the strategy to use, or null to use a {@link ThreadLocalEventBusHolderStrategy}.
+     * @param strategy the strategy to use, or null to use a {@link ThreadLocalEventBusHolderStrategy}
+     * (the default).
      */
     public static synchronized void setStrategy(EventBusHolderStrategy strategy) {
         if (strategy == null) {
